580edf4f3a40a2f62b8dad4290764e807cbc4edd
hanriverkitty <danieltls@naver.com>
2022-07-17 08:45:23 +0900

    let

--- momentum/script.js
+++ momentum/script.js
@@ -1,10 +1,20 @@
 // const 상수, 변하지 않는값
+// let 바뀔수 있는값
 const a = 5;
 const b = 2;
 const hanriver = "123"
+let c = 5;
+let d = 2;
+let hanriver1 = "123"
 console.log(12312312);
 console.log("hello");
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
-console.log("hello " + hanriver)
+console.log("hello " + hanriver);
+console.log(c + d);
+console.log(c * d);
+console.log(c / d);
+console.log("hello " + hanriver1);
+hanriver1 = "456";
+console.log("hello " + hanriver1);
