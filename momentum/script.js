0061b886a87e64bff94286cec76a32428721c5a6
hanriverkitty <danieltls@naver.com>
2022-07-17 08:39:15 +0900

    variable

--- momentum/script.js
+++ momentum/script.js
@@ -1,1 +1,10 @@
-alert("hi")
+// const 상수, 변하지 않는값
+const a = 5;
+const b = 2;
+const hanriver = "123"
+console.log(12312312);
+console.log("hello");
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello " + hanriver)
