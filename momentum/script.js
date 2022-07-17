d57f89845b2e07250137fcddbfec3b6b90fa5c58
hanriverkitty <danieltls@naver.com>
2022-07-17 20:43:17 +0900

    function

--- momentum/script.js
+++ momentum/script.js
@@ -64,3 +64,33 @@ console.log(player);
 
 player.points = player.points + 15;
 console.log(player);
+
+
+function sayHello(nameOfPerson, age) {
+    console.log("Hello my name is " + nameOfPerson + " and I'm " + age);
+}
+sayHello("nico", 10);
+sayHello("dal", 23);
+sayHello("lynn", 21);
+
+function plus(firstNumber, secondNumber) {
+    console.log(firstNumber + secondNumber);
+}
+
+function divide(a, b) {
+    console.log(a / b);
+}
+plus(60, 8);
+divide(98, 20);
+
+const player1 = {
+    name: "nico",
+    sayHello1: function (otherPersonsName) {
+        console.log("hello! " + otherPersonsName + " nice to meet you");
+    },
+};
+
+console.log(player1);
+console.log(player1.name);
+player1.sayHello1("lynn");
+player1.sayHello1("nico");
