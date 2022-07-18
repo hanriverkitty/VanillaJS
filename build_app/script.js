095b0b50b14b111514a3405b119964d7ae52a4a9
hanriverkitty <danieltls@naver.com>
2022-07-18 14:54:19 +0900

    use localstorage

--- build_app/script.js
+++ build_app/script.js
@@ -4,16 +4,16 @@ const loginInput = loginForm.querySelector("input");
 const link = document.querySelector("a");
 const greeting = document.querySelector("#greeting");
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 function onLoginSubmit(event) {
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    localStorage.setItem("username", username);
+    localStorage.setItem(USERNAME_KEY, username);
     //greeting.innerText = "Hello " + username;
-    greeting.innerText = `Hello ${username}`;
-
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+    paintGreetings(username);
     console.log(username);
 
     /*
@@ -25,8 +25,18 @@ function onLoginSubmit(event) {
 
 
 }
+
+function paintGreetings(username) {
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello ${username}`;
+}
 //loginButton.addEventListener("click", handleBtnClick);
-loginForm.addEventListener("submit", onLoginSubmit);
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings(savedUsername);
+}
 
 
 
