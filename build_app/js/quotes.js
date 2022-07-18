3cf2e4c3673dffc547427cda6424a49bedfafce9
hanriverkitty <danieltls@naver.com>
2022-07-18 17:12:37 +0900

    quotes

---
+++ build_app/js/quotes.js
@@ -0,0 +1,42 @@
+const quotes = [
+    {
+        Latin: "A deo vocatus rite paratus.",
+        Korean: "신은 적절하게 준비된 자를 부른다",
+    },
+    {
+        Latin: "Acta Non Verba.",
+        Korean: "말 대신 행동으로",
+    },
+    {
+        Latin: "Age quod agis.",
+        Korean: "니가 지금 하는 것에 최선을 다하라",
+    },
+    {
+        Latin: "Omnium Rerum Principia Parva Sunt.",
+        Korean: "모든 것들은 작게 시작한다.",
+    },
+    {
+        Latin: "Usus magister est optimus.",
+        Korean: "경험은 가장 좋은 스승이다",
+    },
+    {
+        Latin: "Posse vident et possunt.",
+        Korean: "할 수 있다고 보면 할 수 있는 것이다.",
+    },
+    {
+        Latin: "Omnia tempus habent.",
+        Korean: "모든 것은 때가 있다.",
+    },
+    {
+        Latin: "Faber est suae quisque fortunae.",
+        Korean: "운명을 만드는 사람은 자신이다.",
+    },
+    {
+        Latin: "Id imperfectum anet dum confectum erit.",
+        Korean: "끝날 때까지 아직 끝난 것은 아니다",
+    },
+    {
+        Latin: "Otiare, quo melius labores.",
+        Korean: "일을 더 잘하기 위해서 쉬어라",
+    }
+]
