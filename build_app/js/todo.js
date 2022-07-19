ced09545412f31a4d701d379167bc1e83a0e5be1
hanriverkitty <danieltls@naver.com>
2022-07-19 15:12:23 +0900

    paintToDo

--- build_app/js/todo.js
+++ build_app/js/todo.js
@@ -2,12 +2,19 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
-
+function paintToDo(newTodo) {
+    //html요소 추가
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    span.innerText = newTodo;
+    li.appendChild(span);
+    toDoList.appendChild(li);
+}
 
 function handelToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
-
+    paintToDo(newTodo);
 }
 toDoForm.addEventListener("submit", handelToDoSubmit);
