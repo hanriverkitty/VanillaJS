10b67e69533c2a1ab018e3f1bbde7eb459fb8ba9
hanriverkitty <danieltls@naver.com>
2022-07-19 15:32:23 +0900

    delete ToDoList

--- build_app/js/todo.js
+++ build_app/js/todo.js
@@ -2,12 +2,24 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event) {
+    //console.log(event.target.parentElement.innerText);
+    const li = event.target.parentElement;
+    li.remove();
+}
+
+
+
 function paintToDo(newTodo) {
     //html요소 추가
     const li = document.createElement("li");
     const span = document.createElement("span");
     span.innerText = newTodo;
+    const button = document.createElement("button");
+    button.innerText = "X";
+    button.addEventListener("click", deleteToDo);
     li.appendChild(span);
+    li.appendChild(button);
     toDoList.appendChild(li);
 }
 
